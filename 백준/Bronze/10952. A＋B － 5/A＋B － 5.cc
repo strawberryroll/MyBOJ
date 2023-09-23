e1ed3027626bdba844f4e74547926ec12b5a3ea1
Ahyeon Choi <138025938+strawberryroll@users.noreply.github.com>
2023-09-23 13:04:16 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	int A = 1, B = 1;
+
+	while (A + B != 0) {
+		cin >> A >> B;
+		if (A+B != 0)
+			cout << A + B << '\n';
+	}
+	
+
+}
