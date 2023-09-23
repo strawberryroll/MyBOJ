3b25567e1f11c0183d1299dcbbdee146ba55d07d
Ahyeon Choi <138025938+strawberryroll@users.noreply.github.com>
2023-09-23 13:05:56 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 220 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int T;
+	cin >> T;
+
+	int A;
+	int B;
+
+	while (T--) {
+		cin >> A >> B;
+
+		cout << A + B;
+		cout << '\n';
+
+	}
+}
