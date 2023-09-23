ca28c173843936c12697fd75daee7304471ff199
Ahyeon Choi <138025938+strawberryroll@users.noreply.github.com>
2023-09-23 13:04:39 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	int T;
+	int A, B;
+
+	cin >> T;
+	for (int i = 1; i <= T; i++) {
+		cin >> A >> B;
+		cout << "Case #" << i << ": " << A + B <<'\n';
+	}
+
+}
