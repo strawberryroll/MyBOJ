4bcd9f0b25dda622e970ec81737039b14a081494
Ahyeon Choi <138025938+strawberryroll@users.noreply.github.com>
2023-09-23 11:22:27 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+	
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < i; j++)
+			cout << " ";
+		for (int k = N; i < k; k--)
+			cout << "*";
+		cout << '\n';
+	}
+	
+}
