c56a7bd388169fb0bde0aa1ec6c173cdde9b8e51
Ahyeon Choi <138025938+strawberryroll@users.noreply.github.com>
2024-07-13 22:41:26 +0900

    [level 0] Title: 모스부호 (1), Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,11 @@
+function solution(letter) {
+    let morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z' };
+    
+    let keys = Object.keys(morse);
+    return letter.split(' ').map((s) => String.fromCharCode(keys.indexOf(s)+'a'.charCodeAt(0))).join('');
+}
