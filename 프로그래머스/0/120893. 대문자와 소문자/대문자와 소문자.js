c6899d1f5d1e54897d9087dff28abdf796d800f1
Ahyeon Choi <138025938+strawberryroll@users.noreply.github.com>
2024-08-04 23:39:44 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.31 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,8 @@
+function solution(my_string) {
+    let answer = "";
+    for (let s of my_string) {
+        if (s === s.toLowerCase()) {answer += s.toUpperCase()}
+        else {answer += s.toLowerCase()}
+    }
+    return answer;
+}
