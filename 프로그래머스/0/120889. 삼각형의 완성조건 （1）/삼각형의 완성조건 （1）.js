4fd9be70dd7ba4de07293b72cb5ce39e68c3d4e6
Ahyeon Choi <138025938+strawberryroll@users.noreply.github.com>
2024-08-12 21:49:58 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,6 +1,6 @@
 function solution(sides) {
     let max = Math.max(...sides);
     let remain = sides.reduce((acc, cur) => acc+cur, 0) - max;
-    if (remain > max) return 1;
-    else return 2;
+    
+    if (remain > max) return 1; else return 2;
 }
