1362549d329cccd43797de9ce1d5b8f7037b4f65
Ahyeon Choi <138025938+strawberryroll@users.noreply.github.com>
2024-08-13 17:28:47 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,9 +1,11 @@
 function solution(my_string, num1, num2) {
     let arr = [...my_string];
     
-    let temp = arr[num1];
-    arr[num1] = arr[num2];
-    arr[num2] = temp;
+    [arr[num1], arr[num2]] = [arr[num2], arr[num1]];
+    
+    // let temp = arr[num1];
+    // arr[num1] = arr[num2];
+    // arr[num2] = temp;
     
     return arr.join('');
 }
