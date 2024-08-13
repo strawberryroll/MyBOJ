1e976e57d1f2bf93d737258b97cb631f2a64e819
Ahyeon Choi <138025938+strawberryroll@users.noreply.github.com>
2024-08-13 16:51:19 +0900

    [level 0] Title: 369게임, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.js
+++ 프로그래머스/0/120891. 369게임/369게임.js
@@ -1,8 +1,11 @@
 function solution(order) {
     let str = order.toString();
-    let count = 0;
-    for (s of str) {
-        if (s === '3' || s === '6' || s === '9') count++;
-    } 
-    return count;
+    let ans = str.match(/[369]/g);
+    return ans ? ans.length : 0;
+ 
+    // let count = 0;
+    // for (s of str) {
+    //     if (s === '3' || s === '6' || s === '9') count++;
+    // } 
+    // return count;
 }
