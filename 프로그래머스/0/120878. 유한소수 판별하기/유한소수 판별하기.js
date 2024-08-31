a7eed423a558b335d231da0dc2b47df240ebd4a2
Ahyeon Choi <138025938+strawberryroll@users.noreply.github.com>
2024-08-31 20:13:44 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -0,0 +1,16 @@
+function solution(a, b) {
+    for (let i=1; i<=a; i++) {
+        if (a % i === 0 && b % i === 0) {
+            a = a / i; b = b / i;
+        }
+    }
+    
+    while (b % 2 === 0) {
+        b = b / 2;
+    }
+    while (b % 5 === 0) {
+        b = b / 5;
+    }
+    
+    return (b === 1) ? 1 : 2;
+}
