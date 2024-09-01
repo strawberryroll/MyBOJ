ed0d28e530dc214fbf8a534ca1d5ef22e54b104d
Ahyeon Choi <138025938+strawberryroll@users.noreply.github.com>
2024-09-01 21:09:45 +0900

    [level 0] Title: 문자열 밀기, Time: 5.31 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -0,0 +1,29 @@
+function solution(A, B) {
+    let count = 0;
+    let arrA = [...A];
+    
+    for (let i=0; i<A.length; i++) {
+        if (arrA.join("") === B) {
+            return count;
+        }
+        
+        let temp = arrA.pop();
+        arrA.unshift(temp);
+        count++;
+        
+        if (arrA.join("") === B) {
+            return count;
+        }
+        
+        console.log(arrA, count);
+    }
+    
+    if (arrA.join("") !== [...B]) {
+            return -1;
+    }
+    
+    
+    
+    
+    
+}
