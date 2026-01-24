46332bb8d36d022f410879e376a956665b2cfbe5
Ahyeon Choi <138025938+strawberryroll@users.noreply.github.com>
2026-01-25 00:08:13 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 96 ms, Memory: 9496 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+let input = fs
+    .readFileSync(process.platform == "linux" ? "/dev/stdin" : "input.txt")
+    .toString()
+    .trim();
+
+input = parseInt(input);
+
+for (let i = 1; i <= input; i++) {
+    console.log("*".repeat(i));
+}
