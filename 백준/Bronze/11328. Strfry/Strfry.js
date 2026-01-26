fc49d4e805cd5b36b2475b2316bf4621dd65a8c9
Ahyeon Choi <138025938+strawberryroll@users.noreply.github.com>
2026-01-26 20:55:16 +0900

    [Bronze II] Title: Strfry, Time: 220 ms, Memory: 13620 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.js
+++ 백준/Bronze/11328. Strfry/Strfry.js
@@ -9,9 +9,21 @@ const input = fs
 const n = Number(input[0][0]);
 
 for (let i = 0; i < n; i++) {
-    const a = [...input[i + 1][0]].sort().join("");
-    const b = [...input[i + 1][1]].sort().join("");
+    const arr = Array(27).fill(0);
+    const a = input[i + 1][0];
+    const b = input[i + 1][1];
 
-    if (a === b) console.log("Possible");
+    for (const c of a) {
+        arr[c.charCodeAt(0) - 97]++;
+    }
+    for (const c of b) {
+        arr[c.charCodeAt(0) - 97]--;
+    }
+
+    let isPossible = true;
+    for (const n of arr) {
+        if (n != 0) isPossible = false;
+    }
+    if (isPossible) console.log("Possible");
     else console.log("Impossible");
 }
